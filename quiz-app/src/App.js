4406717faafadfeb9dde3b4d996b134b9a2cf770
Annie Uchiha <annie.pirosmani@yahoo.com>
2024-06-23 23:30:14 +0300

    still running into errors

--- quiz-app/src/App.js
+++ quiz-app/src/App.js
@@ -1,16 +1,20 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
 import Home from "./pages/Home";
+import Notes from "./pages/Notes";
 import Quiz from "./pages/Quiz";
 
 function App() {
   return (
-    <Router>
+    <div>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/notes" element={<Notes />} />
         <Route path="/quiz" element={<Quiz />} />
       </Routes>
-    </Router>
+    </div>
   );
 }
 
---
+++ quiz-app/src/pages/Notes.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Notes() {
+  return (
+    <div>
+      <h1>Notes Page</h1>
+    </div>
+  );
+}
+
+export default Notes;
