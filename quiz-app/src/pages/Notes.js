04f07081c09df4495b4c8211024afbaa2010087e
Annie Uchiha <annie.pirosmani@yahoo.com>
2024-06-24 21:48:50 +0300

    working on notes page

--- quiz-app/src/pages/Notes.js
+++ quiz-app/src/pages/Notes.js
@@ -1,10 +1,23 @@
 import React from "react";
+import "./Notes.scss";
 
 function Notes() {
   return (
-    <div>
+    <section className="notes">
       <h1>Notes Page</h1>
-    </div>
+      <div>
+        <h2>HTML5</h2>
+      </div>
+      <div>
+        <h2>CSS3</h2>
+      </div>
+      <div>
+        <h2>JavaScript</h2>
+      </div>
+      <div>
+        <h2>React.js</h2>
+      </div>
+    </section>
   );
 }
 
