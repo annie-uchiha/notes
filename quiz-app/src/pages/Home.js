17bb5bae42bad2c38a0e142de917e5f861b12f49
Annie Uchiha <annie.pirosmani@yahoo.com>
2024-06-25 23:21:36 +0300

    styling the home page

--- quiz-app/src/pages/Home.js
+++ quiz-app/src/pages/Home.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import "./Home.scss";
 
 const Home = () => {
   const history = useNavigate();
 
   return (
-    <div>
-      <h1>Welcome to the Quiz App</h1>
-      <button onClick={() => history.push("/quiz")}>Take the quiz now</button>
+    <div className="home-container">
+      <h1 className="home-container__title">Welcome to the Quiz App</h1>
+      <button className="home-container__btn" onClick={() => history.push("/quiz")}>Take the quiz now</button>
     </div>
   );
 };
