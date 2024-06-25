bd118d43fc53a44d684509df92e33b1eb5ccc337
Annie Uchiha <annie.pirosmani@yahoo.com>
2024-06-25 23:26:44 +0300

    fixed link

--- quiz-app/src/pages/Home.js
+++ quiz-app/src/pages/Home.js
@@ -3,12 +3,14 @@ import { useNavigate } from "react-router-dom";
 import "./Home.scss";
 
 const Home = () => {
-  const history = useNavigate();
+  const navigate = useNavigate();
 
   return (
     <div className="home-container">
       <h1 className="home-container__title">Welcome to the Quiz App</h1>
-      <button className="home-container__btn" onClick={() => history.push("/quiz")}>Take the quiz now</button>
+      <button className="home-container__btn" onClick={() => navigate("/quiz")}>
+        Take the quiz now
+      </button>
     </div>
   );
 };
