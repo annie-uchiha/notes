37a7cec07ae5df8c44a55817c62bb33b31c70d88
Annie Uchiha <annie.pirosmani@yahoo.com>
2024-06-27 21:26:53 +0300

    put styling for dark and light theme in index.css so it applies on the whole page not just parts of it

--- quiz-app/src/App.js
+++ quiz-app/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./pages/Home";
@@ -9,12 +9,16 @@ import "./App.css";
 const App = () => {
   const [theme, setTheme] = useState("light");
 
+  useEffect(() => {
+    document.body.className = theme;
+  }, [theme]);
+
   const toggleTheme = () => {
     setTheme(theme === "light" ? "dark" : "light");
   };
 
   return (
-    <div className={`App ${theme}`}>
+    <div className="App">
       <Header toggleTheme={toggleTheme} theme={theme} />
       <Routes>
         <Route path="/" element={<Home />} />
